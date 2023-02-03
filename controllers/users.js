bc828ce1cb534153e4cbe3838be8a195103461c7
Andrey Salnikov <chijik666777@yandex.ru>
2023-02-03 13:58:20 +0300

    fix

--- controllers/users.js
+++ controllers/users.js
@@ -110,7 +110,7 @@ module.exports.login = (req, res, next) => {
       //   httpOnly: true,
       // });
       // вернём токен дополнительно
-      res.send({ token, jwt });
+      res.send({ token });
     })
     .catch((err) => {
       next(err);
